76c2d2a702be5e248b6162b1253350f9b8cac4d4
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-05-05 09:54:04 +0200

    adding profiling code

---
+++ inst/profile_my_inla.R
@@ -0,0 +1,20 @@
+library(testvirtualclass)
+data(cbpp, package = "lme4")
+meta1 <- 1L
+meta2 <- 2L
+meta3 <- 3L
+meta4 <- 4L
+meta5 <- "Meta 5"
+meta6 <- "Meta 6"
+Rprof()
+test <-       my_inla(
+  data = cbpp,
+  meta1 = meta1,
+  meta2 = meta2,
+  meta3 = meta3,
+  meta4 = meta4,
+  meta5 = meta5,
+  meta6 = meta6
+)
+Rprof(NULL)
+summaryRprof()
